8bdd32a46a391393503d245c58da35e676838c40
Maxwell Novak <novak.max@gmail.com>
2023-04-16 13:58:03 -0400

    Make image tint when clicked

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Container, FederatedPointerEvent, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +9,27 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+const onClick = (_: FederatedPointerEvent): void => {
+	tintImage(0);
+}
 
-clampy.anchor.set(0.5);
+const tintImage = (index: number) => {
+	setTimeout(() => {
+		clampy.tint = 0xFFFFFF - index;
+		if (index < 255) {
+			tintImage(index+1)
+		}
+	}, 20);
+}
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+const container = new Container();
+container.x = 400;
+container.y = 0;
+app.stage.addChild(container);
 
-app.stage.addChild(clampy);
+const clampy = Sprite.from("clampy.png");
+clampy.x = -300;
+clampy.y = 100;
+container.addChild(clampy);
+clampy.on("pointertap", onClick)
+clampy.interactive = true;
